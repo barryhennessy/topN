70e510ef00e4effbfef1f7d90779c0094f895ac8
Barry Hennessy <barryhenn@gmail.com>
2014-02-11 22:27:43 +0000

    IO: tests and class to handle IO for topN

---
+++ io/io.py
@@ -0,0 +1,17 @@
+__author__ = 'barryhennessy'
+
+class IO(object):
+    """Handles IO for TopN."""
+
+    def read(self, path):
+
+        """Reads integers from the path given with a generator
+
+        :param path: The path to the file to be read
+
+        :return: Generator of ints
+        """
+        with open(path, "rU") as input_file:
+            for line in input_file:
+                current_int = int(line)
+                yield current_int
---
+++ test/io/test_io.py
@@ -0,0 +1,44 @@
+from unittest import TestCase
+
+from io import IO
+
+__author__ = 'barryhennessy'
+
+class TestIO(TestCase):
+
+    # This assumes unit tests are being run from the project root directory
+    valid_file = "test/io/valid_file.txt"
+    invalid_file = "test/io/invalid_file.txt"
+
+    def test_throws_if_path_non_readable(self):
+        io = IO()
+        with self.assertRaises(IOError):
+            reader = io.read("path/that/doesn't/exist")
+            for number in reader:
+                pass
+
+    def test_values_integers(self):
+        io = IO()
+        reader = io.read(self.valid_file)
+
+        for number in reader:
+            self.assertIsInstance(number, int)
+
+    def test_non_int_raises_value_error(self):
+        io = IO()
+        reader = io.read(self.invalid_file)
+
+        with self.assertRaises(ValueError):
+            for number in reader:
+                pass
+
+    def test_reads_correct_ints(self):
+        """Tests to ensure the correct numbers are returned
+
+        Implicitly bound to the values in valid_file.txt. Make sure to keep the
+        values in sync or use a different file if diverging often.
+        """
+        io = IO()
+        reader = io.read(self.valid_file)
+
+        self.assertItemsEqual([5, 6, 2, 4, 7, 232, 3], reader)
